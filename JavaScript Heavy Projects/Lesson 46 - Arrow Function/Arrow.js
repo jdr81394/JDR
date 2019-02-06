a50420eaeba05f5cd457ce35c52409d683cd4ae8
jdr81394 <42478354+jdr81394@users.noreply.github.com>
2019-02-06 16:39:32 -0500

    Delete Arrow.js

--- JavaScript Heavy Projects/Lesson 46 - Arrow Function/Arrow.js
+++
@@ -1,8 +0,0 @@
-// used because it allows you to access things that would not normally be able
-// to be accessed from an object.
-// also the this key word isnt needed? idk.
-
-function arrow (param1, param2){
-  alert(param1 + param2)
-}
-arrow(12, 13);
