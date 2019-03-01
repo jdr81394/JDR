3ed941f53c0e3712e99bd8c598a3a2dda5801e26
JDR <42478354+jdr81394@users.noreply.github.com>
2019-03-01 15:26:06 -0500

    email app model built

--- Java Projects/Email App/src/emailapp/Email.java
+++ Java Projects/Email App/src/emailapp/Email.java
@@ -7,42 +7,45 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
-    private int mailboxCapacity;
+    private String email;
+    private int mailboxCapacity = 500;
     private int defaultPasswordLength = 10;
     private String alternateEmail;
+    private String companySuffix = "xyzcompany.com";
 
     //Constructor to receive first name and last name
     public Email(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("Email created: " + this.firstName + " " + this.lastName);
+
 
         //Call a method asking for the department - return the department
         this.department = setDepartment();
-        System.out.println("Department: " + this.department);
+
 
         //Call a method that returns a random password
         this.password = randomPassword(defaultPasswordLength);
-        System.out.println("Your pass word is: " + this.password);
+        System.out.println("Your password is: " + this.password);
+
+        //combine elements to generate email
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+
 
 
     }
     //Ask for the department
-    private String setDepartment(){
-        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Account\n0 for none\n Enter department code");
+    private String setDepartment() {
+        System.out.print("New worker: " + firstName + ". Department Codes:\n1 for Sales\n2 for Development\n3 for Account\n0 for none\n Enter department code");
         Scanner in = new Scanner(System.in);
         int depChoice = in.nextInt();
-        if(depChoice == 1){
-            return "sales";
-        }
-        else if (depChoice == 2){
-            return "dev";
-        }
-        else if (depChoice == 3){
-            return "acct";
-        } else {
-            return "";
-        }
+
+        if(depChoice == 1){ return "sales"; }
+
+        else if (depChoice == 2){ return "dev"; }
+
+        else if (depChoice == 3){ return "acct"; }
+
+        else { return ""; }
 
     }
 
@@ -58,10 +61,42 @@ public class Email {
         return new String(password);
     }
 
+
+
     //Set the mailbox capacity
+    public void setMailboxCapacity(int capacity){ this.mailboxCapacity = capacity; }
 
     //Set the alternate email
+    public void setAlternateEmail(String altEmail){ this.alternateEmail = altEmail; }
 
     //Change the password
+    public void changePassword(String password){ this.password = password; }
+
+
+    public int getMailboxCapacity(){ return mailboxCapacity; }
+
+    public String getAlternateEmail(){ return alternateEmail; }
+
+    public String getPassword() {return password;}
+
+    public String showInfo(){ return "DISPLAY NAME: " + firstName + " " + lastName +
+                                    "\nCOMPANY EMAIL: " + email +
+                                    "\nMAILBOX CAPACITY: " + mailboxCapacity + " mb";}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
